823fe41c2c56960d0eb051b03478e2f0d4c4e8a3
quhailong1995 <quhailong1995@163.com>
2018-01-26 17:12:19 +0800

    ，，，，

--- AbstractFactoryPattern/src/com/quhl/abstractfactory/Provider.java
+++ AbstractFactoryPattern/src/com/quhl/abstractfactory/Provider.java
@@ -6,7 +6,7 @@ package com.quhl.abstractfactory;
  * 这样一旦需要增加新的功能，直接增加新的工厂类就可以了，不需要修改之前的代码。
  *
  * 其实这个模式的好处就是，如果你现在想增加一个功能：发及时信息，则只需做一个实现类，实现Sender接口，
- * 同时做一个工厂类，实现Provider接口，就OK了，无需去改动现成的代码。这样做，拓展性较好！
+ * 同时做一个工厂类，实现Provider接口，就OK了，无需去改动现成的代码。这样做，拓展性较好！！！
  */
 public interface Provider {
     public Sender produce();
