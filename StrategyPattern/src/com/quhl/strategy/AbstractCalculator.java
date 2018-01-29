7f4cdc37762834201521f7838183cea61d6b5d59
quhailong1995 <quhailong1995@163.com>
2018-01-29 10:23:44 +0800

    策略模式

--- StrategyPattern/src/com/quhl/strategy/AbstractCalculator.java
+++ StrategyPattern/src/com/quhl/strategy/AbstractCalculator.java
@@ -5,6 +5,9 @@ package com.quhl.strategy;
  * 需要设计一个接口，为一系列实现类提供统一的方法，多个实现类实现该接口，设计一个抽象类（可有可无，属于辅助类），提供辅助函数
  *
  * AbstractCalculator是辅助类，提供辅助方法
+ *
+ * 策略模式的决定权在用户，系统本身提供不同算法的实现，新增或者删除算法，
+ * 对各种算法做封装。因此，策略模式多用在算法决策系统中，外部用户只需要决定用哪个算法即可。
  */
 public abstract class AbstractCalculator {
 
