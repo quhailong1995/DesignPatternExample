8ec032fa28627317ab7bd7b2bea79b434d3872b2
quhailong1995 <quhailong1995@163.com>
2018-01-29 11:19:13 +0800

    观察者模式

---
+++ ObserverPattern/src/com/quhl/observer/Observer.java
@@ -0,0 +1,5 @@
+package com.quhl.observer;
+
+public interface Observer {
+    public void update();
+}
---
+++ ObserverPattern/src/com/quhl/observer/Observer1.java
@@ -0,0 +1,9 @@
+package com.quhl.observer;
+
+public class Observer1 implements Observer {
+
+    @Override
+    public void update() {
+        System.out.println("observer1 has received!");
+    }
+}
