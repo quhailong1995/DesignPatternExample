ddd269c13c912f253f7c522a7a4d34e2cf55355a
quhailong1995 <quhailong1995@163.com>
2018-01-29 16:25:48 +0800

    访问者模式

---
+++ VisitorPattern/src/com/quhl/visitor/MyVisitor.java
@@ -0,0 +1,8 @@
+package com.quhl.visitor;
+
+public class MyVisitor implements Visitor {
+    @Override
+    public void visit(Subject subject) {
+        System.out.println("visit the subject "+subject.getSubject());
+    }
+}
---
+++ VisitorPattern/src/com/quhl/visitor/Visitor.java
@@ -0,0 +1,5 @@
+package com.quhl.visitor;
+
+public interface Visitor {
+    public void visit(Subject subject);
+}
